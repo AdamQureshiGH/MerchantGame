a24ca69a3872d81b16d0007f8887001418680485
Adam Qureshi <adam.y.qureshi@gmail.com>
2026-04-11 20:26:04 -0400

    Added a travelling system via a for-loop and food that gets consumed every day and a random death message function

--- src/MerchantGame.java
+++ src/MerchantGame.java
@@ -11,6 +11,8 @@ public class MerchantGame
         };
         int currentCityIndex = 0;
         int silver = 100;
+        int health = 100;
+        int foodSupplies = 100;
         boolean isRunning = true;
 
         /*
@@ -33,7 +35,8 @@ public class MerchantGame
             System.out.println(currentCity.getDescription());
 
             System.out.println("\n--- CURRENT STATUS ---");
-            ui.printProgressBar("Health", 100, 100);
+            ui.printProgressBar("Health", health    , 100);
+            ui.printProgressBar("Food Supplies", foodSupplies, 100);
             ui.printProgressBar("Wagon Space", 0, 200);
             ui.printCurrency("Wallet", silver);
 
@@ -52,13 +55,48 @@ public class MerchantGame
                 System.out.println("\n[ Opening Wagon... ]");
             }
             else if (choice == 3) {
-                // travel logic
-                if (currentCity.getNextCityName().equals("END")) {
-                    System.out.println("You have reached the end of the road!");
+                if (currentCity.getNextCityName().equals("FINISHED")) {
+                    System.out.println("You reached the end of the road!");
+                    //Grading system here
                     isRunning = false;
                 } else {
+                    int travelDays = currentCity.getDistance();
                     System.out.println("You begin the " + currentCity.getDistance() + " day journey...");
-                    currentCityIndex++;
+
+                    for(int d = 1; d<= travelDays; d++)
+                    {
+                        System.out.println("Day " + d + " on the road.");
+                        int foodNeeded = (int)(Math.random() * 11) + 10;
+                        if (foodSupplies >= foodNeeded) {
+                            System.out.println(foodNeeded + " units of food consumed.");
+                            foodSupplies -= foodNeeded;
+                            System.out.println("  > Remaining: " + foodSupplies);
+                        } else {
+                            foodSupplies = 0;
+                            health -= 20;
+                            System.out.println("  > !! STARVATION !! You are out of food.");
+                            ui.printProgressBar("Health", health    , 100);
+
+                        }
+
+                        //Random events here
+                        //Rumors here "You hear rumors from a small town that (Town up ahead) is xyz"
+
+                        if (health <= 0) {
+                            System.out.println("\n****************************************");
+                            System.out.println("The journey was too much for you.");
+                            System.out.println("****************************************");
+                            printDeathMessage();
+                            isRunning = false;
+                            break;
+                        }
+                        ui.waitForEnter();
+                    }
+                    if (isRunning) {
+                        currentCityIndex++;
+                        System.out.println("\nWelcome to " + worldMap[currentCityIndex].getName() + "!");
+                    }
+
                 }
             }
             else if (choice == 4) {
@@ -68,12 +106,33 @@ public class MerchantGame
                 // ADD GRADING SYSTEM LATER BASED ON FINAL WEALTH
                 isRunning = false;
             }
+            else if(choice == 415)
+            {
+                System.out.println("Cheater Cheater Pumpkin Eater");
+                 silver = 1000;
+                 health = 100;
+                 foodSupplies = 100;
+            }
 
             if (isRunning) {
                 ui.waitForEnter();
             }
         }
         System.out.println("\n*** Thank you for playing! ***");
+
+    }
+    public static void printDeathMessage() {
+        String[] messages = {
+                "The silk road has claimed another soul. You died of exhaustion.",
+                "Your journey ends here. The elements were harsher than your resolve.",
+                "A final sunset over the dunes. You have perished on the road.",
+                "Hunger and fatigue have won. Your wagon stands abandoned in the mud."
+        };
+
+        int randomIndex = (int)(Math.random() * messages.length);
+        System.out.println("\n****************************************");
+        System.out.println("          GAME OVER");
+        System.out.println("****************************************");
 
     }
 }
